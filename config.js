073fd5cce62ea22e431948f85eaafa10d54d4d0f
Siarhei Puhach <siarhei.puhach@itechart-group.com>
2018-04-02 16:45:00 +0300

    Added images

---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {
+    url: 'https://financerapp.herokuapp.com/',
+    loginUrl: 'https://financerapp.herokuapp.com/users/login',
+    registrationUrl: 'https://financerapp.herokuapp.com/users/register',
+    checkIfloggedInUrl: 'https://financerapp.herokuapp.com/users/isloggedin',
+    logoutUrl: 'https://financerapp.herokuapp.com/users/logout',
+    getItemListUrl: 'https://financerapp.herokuapp.com/items/list',
+    editItemUrl: 'https://financerapp.herokuapp.com/items/',
+    addItemUrl: 'https://financerapp.herokuapp.com/items/add',
+    deleteUrl: 'https://financerapp.herokuapp.com/items/remove'
+}
+
+module.exports = config;
